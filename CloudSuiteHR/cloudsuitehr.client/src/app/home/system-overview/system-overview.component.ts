4fb80e0ffc8bbf2b0f0bf5a7d908653adb23818f
Lucas Barros <lucas.barros@lucastechnologyservice.com>
2024-03-07 14:11:40 +0000

    feat(#1359): creating system overview component

---
+++ CloudSuiteHR/cloudsuitehr.client/src/app/home/system-overview/system-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-overview',
+  templateUrl: './system-overview.component.html',
+  styleUrl: './system-overview.component.css'
+})
+export class SystemOverviewComponent {
+
+}
