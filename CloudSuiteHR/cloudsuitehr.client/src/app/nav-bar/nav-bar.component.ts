0fc8f0bd8d6b9c93cc75525225f6edde5d662505
Lucas Barros <lucas.barros@lucastechnologyservice.com>
2024-03-07 18:27:38 +0000

    feat(#1359): finishing overview navbar variation

--- CloudSuiteHR/cloudsuitehr.client/src/app/nav-bar/nav-bar.component.ts
+++ CloudSuiteHR/cloudsuitehr.client/src/app/nav-bar/nav-bar.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  validationToken: boolean = false;
+  user = 'Lucas Gabriel'
+  userIconText = ""
 
+  validation(){
+    this.validationToken = !this.validationToken;
+    console.log(this.validationToken);
+    this.userIconText = this.getLetters(this.user);
+  }
+
+  getLetters(user: string){
+    const letters = user.split(' ').map(word => word.charAt(0)).join('');
+    return letters;
+  }
 }
