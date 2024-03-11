432ac8605af194efb3ed3cc026bc2c0c7c8f6b71
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-11 15:09:24 -0300

    feat(#1363): creating routes and importing modules

--- CloudSuiteHR/cloudsuitehr.client/src/app/app-routing.module.ts
+++ CloudSuiteHR/cloudsuitehr.client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SystemOverviewComponent } from './home/system-overview/system-overview.component';
+import { CchOverviewComponent } from './cch-overview/cch-overview.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,9 @@ const routes: Routes = [
   },{
     path: 'overview',
     component: SystemOverviewComponent
+  },{
+    path: 'overview/cch',
+    component: CchOverviewComponent
   }
 ];
 
--- CloudSuiteHR/cloudsuitehr.client/src/app/app.module.ts
+++ CloudSuiteHR/cloudsuitehr.client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table'
+import { MatDatepickerModule } from '@angular/material/datepicker'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +11,24 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SystemOverviewComponent } from './home/system-overview/system-overview.component';
+import { CchOverviewComponent } from './cch-overview/cch-overview.component';
+import { MatFormFieldModule, MatHint } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    SystemOverviewComponent
+    SystemOverviewComponent,
+    CchOverviewComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule, MatIconModule
+    AppRoutingModule, MatIconModule,
+    MatTableModule, MatDatepickerModule,
+    MatFormFieldModule, MatInputModule,
+    MatDatepickerModule, MatHint
   ],
   providers: [
     provideAnimationsAsync('noop')
