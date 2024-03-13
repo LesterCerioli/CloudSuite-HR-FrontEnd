d000d51f02948734ad2a1ec183cba8521f271c1d
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-13 11:28:40 -0300

    feat(#1363): adding additional styles

--- CloudSuiteHR/cloudsuitehr.client/src/app/cch-overview/cch-overview.component.ts
+++ CloudSuiteHR/cloudsuitehr.client/src/app/cch-overview/cch-overview.component.ts
@@ -6,27 +6,13 @@ export interface PeriodicElement {
   day: number;
 }
 
-//const ELEMENT_DATA: PeriodicElement[] = getList();
-// [
-//   {month: 1, day: 'Hydrogen'},
-//   {month: 2, day: 'Helium'},
-//   {month: 3, day: 'Lithium'},
-//   {month: 4, day: 'Beryllium'},
-//   {month: 5, day: 'Boron'},
-//   {month: 6, day: 'Carbon'},
-//   {month: 7, day: 'Nitrogen'},
-//   {month: 8, day: 'Oxygen'},
-//   {month: 9, day: 'Fluorine'},
-//   {month: 10, day: 'Neon'},
-// ];
-
 function getList(){
   const numDaysInMonth = (y: number, m: number) => new Date(y, m, 0).getDate();
 
   const formatter = new Intl.DateTimeFormat('pt-br', {month: 'short'});
   const monthName = formatter.format(new Date());
   const numDays = numDaysInMonth(new Date().getFullYear(), new Date().getMonth()+1);
-  console.log(numDays);
+
   let objList = [];
 
   for (let i = 0; i < numDays; i++){
