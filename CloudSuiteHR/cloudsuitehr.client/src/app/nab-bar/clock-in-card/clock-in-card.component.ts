ce9c0a0ca30264ed696d868afa1dbfb1ff31d9c7
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-14 15:03:42 -0300

    feat(#1363): building clock in card component

---
+++ CloudSuiteHR/cloudsuitehr.client/src/app/nab-bar/clock-in-card/clock-in-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clock-in-card',
+  templateUrl: './clock-in-card.component.html',
+  styleUrl: './clock-in-card.component.css'
+})
+export class ClockInCardComponent {
+
+}
