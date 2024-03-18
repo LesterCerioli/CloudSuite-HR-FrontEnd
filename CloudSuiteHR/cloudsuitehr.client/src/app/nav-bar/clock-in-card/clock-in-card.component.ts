daf1d94028ef929c5ea81e0f4ecf13969aefa204
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-03-18 16:14:26 -0300

    feat(#1363): finishing clock in card component

--- CloudSuiteHR/cloudsuitehr.client/src/app/nav-bar/clock-in-card/clock-in-card.component.ts
+++ CloudSuiteHR/cloudsuitehr.client/src/app/nav-bar/clock-in-card/clock-in-card.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 
+export interface PeriodicElement {
+  in: string;
+  out: string;
+  total: string;
+}
+
+
 @Component({
   selector: 'app-clock-in-card',
   templateUrl: './clock-in-card.component.html',
@@ -8,5 +15,6 @@ import { provideNativeDateAdapter } from '@angular/material/core';
   providers: [provideNativeDateAdapter()],
 })
 export class ClockInCardComponent {
-
+  displayedColumns: string[] = ['check_in', 'check_out', 'sub_total'];
+  dataSource = [{in: "09:00", out: "12:05", total: "03:05"},{in: "13:01", out: "17:05", total: "XX:XX"}];
 }
